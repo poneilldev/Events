957af7eeb0f380a3350c1e79d5abff70ae94f657
poneilldev <p.oneill.apc@gmail.com>
2016-05-20 11:00:54 -0600

    code clean up

--- Zanga_0.2/CreateEventViewController.swift
+++ Zanga_0.2/CreateEventViewController.swift
@@ -20,7 +20,6 @@ class CreateEventViewController: UIViewController {
         self.navigationController?.navigationBar.shadowImage = UIImage()
         self.navigationController?.navigationBar.setBackgroundImage(UIImage(), forBarMetrics: UIBarMetrics.Default)
         self.navigationController?.navigationBar.backgroundColor = UIColor.backGroundBlack()
-        //self.navigationController?.navigationBar.barStyle = UIBarStyle.BlackTranslucent
         self.navigationController?.navigationBar.tintColor = UIColor.whiteColor()
         self.navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.myRed()]
         
