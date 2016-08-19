73a4b2186208b0def9356d59f01ff485e2b1112a
poneilldev <p.oneill.apc@gmail.com>
2016-08-19 12:25:26 -0700

    ....

--- Zanga_0.2/CreateEventViewController.swift
+++ Zanga_0.2/CreateEventViewController.swift
@@ -10,11 +10,24 @@ import UIKit
 
 class CreateEventViewController: UIViewController {
 
+    
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var locationTextField: UITextField!
+    @IBOutlet weak var descriptionTextArea: UITextView!
+    @IBOutlet weak var eventImageView: UIImageView!
+    @IBOutlet weak var inviteFriendsButton: UIButton!
+    @IBOutlet weak var createEvent: UIButton!
+    @IBOutlet weak var cancelForm: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        datePicker.setValue(UIColor.whiteColor(), forKey: "textColor")
         self.tabBarController?.tabBar.barTintColor = UIColor.backGroundBlack()
         self.tabBarController?.tabBar.tintColor = UIColor.myRed()
+        
         
         self.navigationController?.navigationBar.barTintColor = UIColor(red: 30.0/255.0, green: 30.0/255.0, blue: 30.0/255.0, alpha: 1.0)
         self.navigationController?.navigationBar.shadowImage = UIImage()
@@ -22,6 +35,7 @@ class CreateEventViewController: UIViewController {
         self.navigationController?.navigationBar.backgroundColor = UIColor.backGroundBlack()
         self.navigationController?.navigationBar.tintColor = UIColor.whiteColor()
         self.navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.myRed()]
+        
         
         // Do any additional setup after loading the view.
     }
---
+++ Zanga_0.2/myExtensions.swift
@@ -0,0 +1,41 @@
+//
+//  myExtensions.swift
+//  Zanga_0.2
+//
+//  Created by Paul O'Neill on 8/19/16.
+//  Copyright © 2016 Paul O'Neill. All rights reserved.
+//
+
+import Foundation
+
+
+extension String {
+    
+    /// Percent escapes values to be added to a URL query as specified in RFC 3986
+    ///
+    /// This percent-escapes all characters besides the alphanumeric character set and "-", ".", "_", and "~".
+    ///
+    /// http://www.ietf.org/rfc/rfc3986.txt
+    ///
+    /// :returns: Returns percent-escaped string.
+    
+    func stringByAddingPercentEncodingForURLQueryValue() -> String? {
+        let allowedCharacters = NSCharacterSet(charactersInString: "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._~")
+        
+        return self.stringByAddingPercentEncodingWithAllowedCharacters(allowedCharacters)
+    }
+    
+}
+
+
+extension Dictionary {
+    func stringFromHttpParameters() -> String {
+        let parameterArray = self.map { (key, value) -> String in
+            let percentEscapedKey = (key as! String).stringByAddingPercentEncodingForURLQueryValue()!
+            let percentEscapedValue = (value as! String).stringByAddingPercentEncodingForURLQueryValue()!
+            return "\(percentEscapedKey)=\(percentEscapedValue)"
+        }
+        
+        return parameterArray.joinWithSeparator("&")
+    }
+}
